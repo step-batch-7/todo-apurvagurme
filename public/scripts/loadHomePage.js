0650539252f7b3964edc3aab892686e0b05bbeea
Apurva <apurva.gurme@thoughtworks.com>
2020-02-07 10:42:39 +0530

    Modified UI

--- public/scripts/loadHomePage.js
+++ public/scripts/loadHomePage.js
@@ -3,9 +3,10 @@ const getSubTasks = function(tasks) {
     let status;
     task.status === true ? (status = 'checked') : (status = '');
     return `
-    <div style="padding-left: 5px; class="cbDiv">
-    <input type="checkbox" onclick="onCheck(event)" class="cb" value=${task.id} ${status}>${task.subTask}
-     <img class="remove" onclick="deleteSubtask(event)" src="./images/remove.svg" id=${task.id} ${status}/>
+    <div style = "padding-left: 5px; display: flex;" class = "cbDiv">
+    <input type="checkbox" onclick="onCheck(event)" class="cb" value=${task.id} ${status}>
+    ${task.subTask}
+     <img class="remove" onclick="deleteSubtask(event)" src="./images/cross.svg" id=${task.id} ${status}/>
     </div>
     <br>`;
   });

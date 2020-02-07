e7a9b70f5fe419b67ae460843d8181ff9412ad3a
Apurva <apurva.gurme@thoughtworks.com>
2020-02-07 11:48:08 +0530

    Displaying save button and changed the title to input type and modified UI a little

--- public/scripts/loadHomePage.js
+++ public/scripts/loadHomePage.js
@@ -6,29 +6,31 @@ const getSubTasks = function(tasks) {
     <div style = "padding-left: 5px; display: flex;" class = "cbDiv">
     <input type="checkbox" onclick="onCheck(event)" class="cb" value=${task.id} ${status}>
     ${task.subTask}
-     <img class="remove" onclick="deleteSubtask(event)" src="./images/cross.svg" id=${task.id} ${status}/>
+    <img class="remove" onclick="deleteSubtask(event)" src="./images/cross.svg" id=${task.id} ${status}/>
     </div>
     <br>`;
   });
   return subTasks.join('\n');
 };
 
-const toHTML = function(context, todo) {
+const toHTML = function(taskTemplate, todo) {
   let subtasks = 'no subtasks';
   if (todo.tasks) {
     subtasks = getSubTasks(todo.tasks);
   }
-  context += `
+  taskTemplate += `
   <div id=${todo.id} class='task' style="margin: 10px;">
-  <div class="titleOfTodo">${todo.title}<p>
+  <div class="titleOfTodo">
+  <input class="titleInputBox" type="text" value=${todo.title} >
   <div> 
-    <img class="editTitle" onclick="edit(event)" src="./images/edit.svg" id=${todo.id}>
-    <img class="removeTodo" onclick="deleteTodo(event)" src="./images/dustbin.svg" id=${todo.id}>
-    </div>
-    </div>
-    <p><div class='scrollTasks'>${subtasks}</div></p>
-    </div>`;
-  return context;
+  <img class="saveButton" onclick="saveReq(event)" src="./images/save.png" id=${todo.id}>
+  <img class="editTitle" onclick="edit(event)" src="./images/edit.svg" id=${todo.id}>
+  <img class="removeTodo" onclick="deleteTodo(event)" src="./images/dustbin.svg" id=${todo.id}>
+  </div>
+  </div>
+  <p><div class='scrollTasks'>${subtasks}</div></p>
+  </div>`;
+  return taskTemplate;
 };
 
 const createTasksTemplate = function(todoList) {

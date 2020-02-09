7a43dc9cb0ec80aecb201796b228f944629924a2
Apurva <apurva.gurme@thoughtworks.com>
2020-02-09 19:38:17 +0530

    Modified search action and added test cases for not found and getTodoList

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -12,3 +12,20 @@ describe('GET', function() {
     });
   });
 });
+
+describe('GET', () => {
+  it('should give 404 statusCode if any one give absent file', done => {
+    request(app.serve.bind(app))
+      .get('/absentFile.js')
+      .expect(404, done);
+  });
+});
+
+describe('GET/getAllTodo', function() {
+  it('should return all existing todoList', function(done) {
+    request(app.serve.bind(app))
+      .get('/todoList')
+      .expect('Content-type', 'text/plain')
+      .expect(200, done);
+  });
+});

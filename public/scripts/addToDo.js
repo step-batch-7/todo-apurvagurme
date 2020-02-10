19a6bd90df167209d15770dfd7e57915dafca76f
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-10 20:05:46 +0530

    [Apurva|Sid] Changed the contract between server-client
    Changed client's data which was string into JSON string
    Previously client request data was a uri encoded string,
    now its a JSON string.

--- public/scripts/addToDo.js
+++ public/scripts/addToDo.js
@@ -15,44 +15,43 @@ const addItem = function() {
 };
 
 const onCheck = function() {
-  const clickedCb = `id=${event.target.parentElement.id}`;
+  const id = event.target.parentElement.id;
   const xhr = new XMLHttpRequest();
   xhr.open('POST', '/toggleTaskStatus', false);
-  xhr.send(clickedCb);
+  xhr.send(JSON.stringify({id}));
 };
 
 const deleteSubtask = function() {
-  const deleted = `id=${event.target.parentElement.id}`;
-  sendPostReq('/deleteSubtask', deleted, renderTodoCollection);
+  const id = event.target.parentElement.id;
+  sendPostReq('/deleteSubtask', JSON.stringify({id}), renderTodoCollection);
 };
 
 const deleteTodo = function() {
-  const deleted = `id=${event.target.parentElement.parentElement.parentElement.id}`;
-  sendPostReq('/deleteTodo', deleted, renderTodoCollection);
+  const id = event.target.parentElement.parentElement.parentElement.id;
+  sendPostReq('/deleteTodo', JSON.stringify({id}), renderTodoCollection);
 };
 
 const saveNewTodo = function() {
-  // look back
   if (event.key === 'Enter') {
     const title = event.target.value;
     event.target.value = '';
-    sendPostReq('/newTodo', `title=${title}`, renderTodoCollection);
+    sendPostReq('/newTodo', JSON.stringify({title}), renderTodoCollection);
   }
 };
 
 const saveNewTitle = function() {
   if (event.key === 'Enter') {
-    const newTitle = `title=${event.target.value}`;
-    const titleId = `id=${event.target.parentElement.parentElement.id}`;
-    sendPostReq('/saveNewTitle', `${newTitle}&${titleId}`, renderTodoCollection);
+    const title = event.target.value;
+    const id = event.target.parentElement.parentElement.id;
+    sendPostReq('/saveNewTitle', JSON.stringify({title, id}), renderTodoCollection);
   }
 };
 
 const editSubtask = function() {
   if (event.key === 'Enter') {
-    const subtask = `subtask=${event.target.value}`;
-    const subtaskId = `todoId=${event.target.parentElement.id}`;
-    sendPostReq('/saveSubTask', `${subtask}&${subtaskId}`, renderTodoCollection);
+    const subtask = event.target.value;
+    const id = event.target.parentElement.id;
+    sendPostReq('/saveSubTask', JSON.stringify({subtask, id}), renderTodoCollection);
   }
 };
 
@@ -60,21 +59,20 @@ const addSubtask = function() {
   if (event.key === 'Enter') {
     const id = event.target.parentElement.parentElement.id;
     const subtask = event.target.value;
-    sendPostReq('/addSubtask', `subtask=${subtask}&id=${id}`, renderTodoCollection);
+    sendPostReq('/addSubtask', JSON.stringify({id, subtask}), renderTodoCollection);
   }
 };
 
 const searchTodo = function() {
   const requiredText = event.target.value;
-  sendPostReq('/searchTodo', `title=${requiredText}`, renderTodoCollection);
+  sendPostReq('/searchTodo', JSON.stringify({title: requiredText}), renderTodoCollection);
 };
 
 const searchSubtask = function() {
   const requiredSubtask = event.target.value;
-  sendPostReq('/searchSubtasks', `subtask=${requiredSubtask}`, renderTodoCollection);
+  sendPostReq('/searchSubtasks', JSON.stringify({subtask: requiredSubtask}), renderTodoCollection);
 };
 
-//look back
 const main = function() {
   addItem();
   addTodo();

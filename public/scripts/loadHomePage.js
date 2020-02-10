c2a2d682119f87f84ff769ddf6bdcf6009d0c19c
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-10 18:22:10 +0530

    [Apurva|Sudipta] Refactored getSubtasks function
    Renamed getSubtasks to generateHtml
    Shifted responsibility of gathering all tasks html from getSubtasks
    function to generateTodoHtml function

--- public/scripts/loadHomePage.js
+++ public/scripts/loadHomePage.js
@@ -1,19 +1,15 @@
-const getSubTasks = function(tasks) {
-  const subTasks = tasks.map(task => {
-    //  look back
-    let status;
-    task.status === true ? status = 'checked' : status = '';
-    return `
+const generateTaskHtml = function(task) {
+  const status = task.status === true ? 'checked' : '';
+  return `
     <div class="cbDiv" id=${task.subtaskID}>
     <input type="checkbox" onclick="onCheck(event)" class="cb" ${status}>
     <input class="titleInputBox" type="text" onkeydown="editSubtask(event)" value="${task.subTask}">
     <img class="remove" onclick="deleteSubtask(event)" src="./images/cross.svg" />
     </div>`;
-  });
-  return subTasks.join('\n');
 };
 
 const generateTodoHtml = function(todo) {
+  const tasks = todo.tasks.map(generateTaskHtml).join('\n');
   return`
   <div id=${todo.id} class='task'>
   <div class="titleOfTodo">
@@ -22,7 +18,7 @@ const generateTodoHtml = function(todo) {
   <img class="removeTodo" onclick="deleteTodo(event)" src="./images/dustbin.svg">
   </div>
   </div>
-  <p><div class='scrollTasks'>${getSubTasks(todo.tasks)}</div></p>
+  <p><div class='scrollTasks'>${tasks}</div></p>
   <div>
   <input class='subtaskInput' type='text' onkeydown="addSubtask(event)" placeholder="Add subtask">
   </div>

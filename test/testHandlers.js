8235e472526ae01fcc84e4295bcde13997d2db88
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-12 12:42:33 +0530

    [Apurva|Sudipta] Removed order dependancy of tests

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -12,13 +12,23 @@ const testData = [
 ];
 
 describe('handlers', function(){
-  after(function(){
+  afterEach(function(){
     writeFileSync(`${__dirname}/resources/testData.json`, JSON.stringify(testData, null, 2));
   });
   describe('GET', function() {
     it('/<staticFilePath> should serve the static file', function(done) {
       request(app.serve.bind(app))
         .get('/index.html')
+        .expect(/TODO LIST/)
+        .expect('content-type', 'text/html')
+        .expect('content-length', '928')
+        .expect('date', /./)
+        .expect(200, done);
+    });
+
+    it('/ should serve index.html', function(done) {
+      request(app.serve.bind(app))
+        .get('/')
         .expect(/TODO LIST/)
         .expect('content-type', 'text/html')
         .expect('content-length', '928')
@@ -62,10 +72,10 @@ describe('handlers', function(){
     it('/renameTodo should rename the todo title of the given id', function(done) {
       request(app.serve.bind(app))
         .post('/renameTodo')
-        .send({todoTitle: 'newName', todoId: '1'})
-        .expect('[{"title":"newName","id":"1","tasks":[]},{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
+        .send({todoTitle: 'newName', todoId: '0'})
+        .expect('[{"title":"newName","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
         .expect('content-type', 'application/json')
-        .expect('content-length', '121')
+        .expect('content-length', '82')
         .expect('date', /./)
         .expect(200, done);
     });
@@ -73,10 +83,10 @@ describe('handlers', function(){
     it('/deleteTodo should delete the todo of the given id', function(done) {
       request(app.serve.bind(app))
         .post('/deleteTodo')
-        .send({ todoId: '1'})
-        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
+        .send({ todoId: '0'})
+        .expect('[]')
         .expect('content-type', 'application/json')
-        .expect('content-length', '81')
+        .expect('content-length', '2')
         .expect('date', /./)
         .expect(200, done);
     });
@@ -88,17 +98,6 @@ describe('handlers', function(){
         .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true},{"name":"newTask","id":"0_1","status":false}]}]')
         .expect('content-type', 'application/json')
         .expect('content-length', '126')
-        .expect('date', /./)
-        .expect(200, done);
-    });
-
-    it('/deleteTask should delete specified task of the specified todo', function(done) {
-      request(app.serve.bind(app))
-        .post('/deleteTask')
-        .send({taskId: '0_1', todoId: '0'})
-        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '81')
         .expect('date', /./)
         .expect(200, done);
     });
@@ -118,9 +117,20 @@ describe('handlers', function(){
       request(app.serve.bind(app))
         .post('/toggleTaskStatus')
         .send({taskId: '0_0', todoId: '0'})
-        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"mango","id":"0_0","status":false}]}]')
+        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":false}]}]')
         .expect('content-type', 'application/json')
         .expect('content-length', '82')
+        .expect('date', /./)
+        .expect(200, done);
+    });
+
+    it('/deleteTask should delete specified task of the specified todo', function(done) {
+      request(app.serve.bind(app))
+        .post('/deleteTask')
+        .send({taskId: '0_0', todoId: '0'})
+        .expect('[{"title":"fruits","id":"0","tasks":[]}]')
+        .expect('content-type', 'application/json')
+        .expect('content-length', '40')
         .expect('date', /./)
         .expect(200, done);
     });

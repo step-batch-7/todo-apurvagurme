d554ee2f67a18e646d7f7a8fbce6bc5bf4fc0b4b
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-12 14:31:19 +0530

    [Apurva|Sudipta] Added server side test for request fields
    Previously request fields were not getting checked at server side.
    Now validateFields handler will check it and give bad request if
    required fields are not given.

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -66,84 +66,170 @@ describe('handlers', function(){
       sinon.replace(fs, 'readFileSync', stubbedReader);
       sinon.replace(fs, 'writeFileSync', () => {});
     });
+
     this.afterAll(function(){
       sinon.restore();
     });
-    it('/todoList should serve saved todo list as JSON', function(done) {
-      request(app.serve.bind(app))
-        .post('/addTodo')
-        .send({todoTitle: 'newTodo'})
-        .expect('[{"title":"newTodo","id":"1","tasks":[]},{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '121')
-        .expect('date', /./)
-        .expect(200, done);
-    });
-    
-    it('/renameTodo should rename the todo title of the given id', function(done) {
-      request(app.serve.bind(app))
-        .post('/renameTodo')
-        .send({todoTitle: 'newName', todoId: '0'})
-        .expect('[{"title":"newName","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '82')
-        .expect('date', /./)
-        .expect(200, done);
+
+    describe('addTodo', function() {
+      it('should add the specified todo when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/addTodo')
+          .send({todoTitle: 'newTodo'})
+          .expect('[{"title":"newTodo","id":"1","tasks":[]},{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '121')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/addTodo')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
     });
 
-    it('/deleteTodo should delete the todo of the given id', function(done) {
-      request(app.serve.bind(app))
-        .post('/deleteTodo')
-        .send({ todoId: '0'})
-        .expect('[]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '2')
-        .expect('date', /./)
-        .expect(200, done);
+    describe('renameTodo', function() {
+      it('should rename the todo title of the given id when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/renameTodo')
+          .send({todoTitle: 'newName', todoId: '0'})
+          .expect('[{"title":"newName","id":"0","tasks":[{"name":"apple","id":"0_0","status":true}]}]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '82')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/renameTodo')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
     });
 
-    it('/addTask should add task to the specified todo', function(done) {
-      request(app.serve.bind(app))
-        .post('/addTask')
-        .send({taskName: 'newTask', todoId: '0'})
-        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true},{"name":"newTask","id":"0_1","status":false}]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '126')
-        .expect('date', /./)
-        .expect(200, done);
+    describe('deleteTodo', function() {
+      it('should delete the todo of the given id when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/deleteTodo')
+          .send({ todoId: '0'})
+          .expect('[]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '2')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/deleteTodo')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
     });
 
-    it('/renameTask should add task to the specified todo', function(done) {
-      request(app.serve.bind(app))
-        .post('/renameTask')
-        .send({newName: 'mango', taskId: '0_0', todoId: '0'})
-        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"mango","id":"0_0","status":true}]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '81')
-        .expect('date', /./)
-        .expect(200, done);
+    describe('addTask', function() {
+      it('should add task to the specified todo when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/addTask')
+          .send({taskName: 'newTask', todoId: '0'})
+          .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":true},{"name":"newTask","id":"0_1","status":false}]}]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '126')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/addTask')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
     });
 
-    it('/toggleTaskStatus should add task to the specified todo', function(done) {
-      request(app.serve.bind(app))
-        .post('/toggleTaskStatus')
-        .send({taskId: '0_0', todoId: '0'})
-        .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":false}]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '82')
-        .expect('date', /./)
-        .expect(200, done);
+    describe('renameTask', function() {
+      it('should add task to the specified todo when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/renameTask')
+          .send({newName: 'mango', taskId: '0_0', todoId: '0'})
+          .expect('[{"title":"fruits","id":"0","tasks":[{"name":"mango","id":"0_0","status":true}]}]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '81')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/renameTask')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
     });
 
-    it('/deleteTask should delete specified task of the specified todo', function(done) {
-      request(app.serve.bind(app))
-        .post('/deleteTask')
-        .send({taskId: '0_0', todoId: '0'})
-        .expect('[{"title":"fruits","id":"0","tasks":[]}]')
-        .expect('content-type', 'application/json')
-        .expect('content-length', '40')
-        .expect('date', /./)
-        .expect(200, done);
+    describe('toggleTaskStatus', function() {
+      it('should add task to the specified todo when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/toggleTaskStatus')
+          .send({taskId: '0_0', todoId: '0'})
+          .expect('[{"title":"fruits","id":"0","tasks":[{"name":"apple","id":"0_0","status":false}]}]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '82')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/toggleTaskStatus')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
+    });
+
+    describe('deleteTask', function() {
+      it('should delete specified task of the specified todo when required fields are given', function(done) {
+        request(app.serve.bind(app))
+          .post('/deleteTask')
+          .send({taskId: '0_0', todoId: '0'})
+          .expect('[{"title":"fruits","id":"0","tasks":[]}]')
+          .expect('content-type', 'application/json')
+          .expect('content-length', '40')
+          .expect('date', /./)
+          .expect(200, done);
+      });
+
+      it('should response "bad request" when required fields are not given', function(done) {
+        request(app.serve.bind(app))
+          .post('/deleteTask')
+          .send({})
+          .expect('')
+          .expect('content-length', '0')
+          .expect('date', /./)
+          .expect(400, done);
+      });
     });
 
     it('/<invalidAction> should give 404 and not found message', function(done) {
@@ -156,17 +242,17 @@ describe('handlers', function(){
         .expect(404, done);
     });
   });
-});
 
-describe('<not-allowedMethod>', function() {
-  it('/todoList should serve saved todo list as JSON', function(done) {
-    request(app.serve.bind(app))
-      .put('/path')
-      .send({todoTitle: 'newTodo'})
-      .expect('<html><body><h1>Method Not Allowed</h1></body></html>')
-      .expect('content-type', 'text/html')
-      .expect('content-length', '53')
-      .expect('date', /./)
-      .expect(405, done);
+  describe('<not-allowedMethod>', function() {
+    it('/todoList should serve saved todo list as JSON', function(done) {
+      request(app.serve.bind(app))
+        .put('/path')
+        .send({todoTitle: 'newTodo'})
+        .expect('<html><body><h1>Method Not Allowed</h1></body></html>')
+        .expect('content-type', 'text/html')
+        .expect('content-length', '53')
+        .expect('date', /./)
+        .expect(405, done);
+    });
   });
 });

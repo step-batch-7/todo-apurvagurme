775a689428efba965e2d423be39974eef66e1069
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-13 17:20:48 +0530

    [Apurva|Sudipta] now path mathing in app class is a strict match

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  DATA_STORE_PATH: process.env.DATA_STORE_PATH || `${__dirname}/data/todoData.json`
+  DATA_STORE_PATH: process.env.DATA_STORE_PATH || `${__dirname}/data/todoData.json`,
+  USERS_INFO_PATH: process.env.USERS_INFO_PATH || `${__dirname}/data/users.json`
 };

0238ebfdcb874040676c869404e5ada88ed5ee74
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-17 14:37:42 +0530

    [Apurva|Sudipta] minor display bugs of todo

--- public/scripts/loadHomePage.js
+++ public/scripts/loadHomePage.js
@@ -1,33 +1,40 @@
 const generateTaskHtml = function(task) {
   const status = task.status === true ? 'checked' : '';
-  return `
-    <div class="taskDiv" id=${task.id}>
+  const taskHtml = document.createElement('div');
+  taskHtml.className = 'taskDiv';
+  taskHtml.id = task.id;
+  taskHtml.innerHTML = `
       <input type="checkbox" oninput="toggleTaskStatus('${task.id}')" id="cb${task.id}" class="taskCheckBox" ${status}>
       <label for="cb${task.id}"><div></div></label>
-      <p contenteditable=true onkeydown="blurOnEnter(this)" onblur="renameTask(this,'${task.id}')">${task.name}</p>
-      <img onclick="deleteTask('${task.id}')" src="images/cross.svg" />
-    </div>`;
+      <p contenteditable=true onkeydown="blurOnEnter(this)" onblur="renameTask(this,'${task.id}')"></p>
+      <img onclick="deleteTask('${task.id}')" src="images/cross.svg" />`;
+  taskHtml.children[2].innerText = task.name;
+  return taskHtml;
 };
 
 const generateTodoHtml = function(todo) {
-  const tasks = todo.tasks.map(generateTaskHtml).join('\n');
-  return`
-    <div id=${todo.id} class='todo'>
+  const tasks = todo.tasks.map(generateTaskHtml);
+  const todoHtml = document.createElement('div');
+  todoHtml.className = 'todo';
+  todoHtml.id = todo.id;
+  todoHtml.innerHTML = `
       <div class="todoTitle">
         <h3 class="titleInputBox" contenteditable=true onkeydown="blurOnEnter(this)" 
           onblur="renameTodo(this,'${todo.id}')">${todo.title}</h3>
         <img class="removeTodo" onclick="deleteTodo('${todo.id}')" src="./images/dustbin.svg">
       </div>
-      <div class='tasksContainer'>${tasks}</div>
-      <input class='subtaskInput' type='text' onkeydown="addTask('${todo.id}')" placeholder="New Task">
-    </div>`;
+      <div class='tasksContainer'></div>
+      <input class='subtaskInput' type='text' onkeydown="addTask('${todo.id}')" placeholder="New Task">`;
+  tasks.forEach(task => todoHtml.children[1].append(task));
+  return todoHtml;
 };
 
 const renderTodoList = function(todoList) {
   const list = JSON.parse(todoList);
-  const html = list.map(generateTodoHtml).join('\n');
+  const todoHtmls = list.map(generateTodoHtml);
   const container = document.getElementById('todoListContainer');
-  container.innerHTML = html;
+  container.innerHTML = '';
+  todoHtmls.forEach(todo => container.append(todo));
 };
 
 const loadAllTodoLists = function() {

1fc3a756bd2cd3840bc18417f9bfa3afdf119295
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-20 11:50:08 +0530

    fixed minor bug

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./lib/app');
 const {saveUsersData, saveAllTodoLists, allTodoLists, usersData} = require('./lib/dataStore');
 
 const main = function(){
-  port = process.env.PORT || 8000;
+  const port = process.env.PORT || 8000;
   app.locals.allTodoLists = allTodoLists;
   app.locals.usersData = usersData;
   app.locals.saveAllTodoLists = saveAllTodoLists;

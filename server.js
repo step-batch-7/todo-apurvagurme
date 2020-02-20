e8fbc66ad68b0edd4abe60ada2a8a6cedc15ff7d
Sudipta Kundu <58025792+mildshower@users.noreply.github.com>
2020-02-20 11:35:24 +0530

    [Apurva|Sudipta] added feature taking port number from cli

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const app = require('./lib/app');
 const {saveUsersData, saveAllTodoLists, allTodoLists, usersData} = require('./lib/dataStore');
 
 const main = function(){
+  port = process.env.PORT || 8000;
   app.locals.allTodoLists = allTodoLists;
   app.locals.usersData = usersData;
   app.locals.saveAllTodoLists = saveAllTodoLists;
   app.locals.saveUsersData = saveUsersData;
-  app.listen(8000);
+  app.listen(port, () => {
+    console.log(`Started listening at ${port}`);
+  });
 };
 
 main();
